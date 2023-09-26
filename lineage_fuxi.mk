7a9709aef9d5b69f6cfd6fab9cd0a775b693d0bd
jgudec <jakov.gudec@gmail.com>
2023-09-26 11:21:18 +0100

    crDroid bringup

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -15,7 +15,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/fuxi/device.mk)
 
 # Inherit from Gapps
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+ifneq ($(NO_GMS),true)
+$(call inherit-product, vendor/gms/products/gms.mk)
+endif
 
 ## Device identifier
 PRODUCT_DEVICE := fuxi
@@ -24,8 +26,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133G
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_SYSTEM_NAME := 2211133G
-PRODUCT_SYSTEM_DEVICE := 2211133G
+PRODUCT_SYSTEM_NAME := Xiaomi 13
+PRODUCT_SYSTEM_DEVICE := Xiaomi 13
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="fuxi_global-user 13 TKQ1.220905.001 V14.0.9.0.TMCMIXM release-keys"
@@ -34,3 +36,8 @@ BUILD_FINGERPRINT := Xiaomi/fuxi_global/fuxi:13/TKQ1.220905.001/V14.0.9.0.TMCMIX
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# crDroid flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_EXCLUDES_AUDIOFX := true
+
