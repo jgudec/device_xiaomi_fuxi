6a297d88267c52ee37b1c757f0e36e85aa57dcb2
jgudec <jakov.gudec@gmail.com>
2023-10-10 13:43:21 +0100

    fuxi: overlay: Generalize overlay directory

--- device.mk
+++ device.mk
@@ -339,7 +339,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
