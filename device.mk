3842255dd0fa6d537d19f8a13ce5af5f42b96aae
lolipuru <banstellazz@gmail.com>
2023-10-19 01:39:47 +0700

    fuxi: some fixup for overlay
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -345,11 +345,10 @@ PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
     FrameworksResTargetFuxi \
-    FrameworksResTargetXiaomi \
     SettingsOverlayChina \
     SettingsOverlayGlobal \
     SettingsProviderOverlay \
-    SettingsResCommonXiaomi \
+    SettingsResCommon \
     SystemUIResCommon \
     SystemUIResCommonFuxi \
     TelecommResCommon \
