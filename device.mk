52461f67a9f6dc2200ae1f73ef609e938d54ced9
jgudec <jakov.gudec@gmail.com>
2024-02-24 18:36:42 +0000

    fuxi: Initial EvolutionX bringup

--- device.mk
+++ device.mk
@@ -349,7 +349,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_PACKAGES += \
     CarrierConfigRes \
--- evolution_fuxi.mk
+++ evolution_fuxi.mk
@@ -8,18 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common EvolutionX configurations
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/fuxi/device.mk)
 
-# Inherit from Gapps
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-
 ## Device identifier
 PRODUCT_DEVICE := fuxi
-PRODUCT_NAME := lineage_fuxi
+PRODUCT_NAME := evolution_fuxi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := 2211133G
 PRODUCT_MANUFACTURER := Xiaomi
